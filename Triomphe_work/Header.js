18cbfff9873d00631e1b1a1fb07734e6d90974d8
Triomphe-spark <triomphesognikin@gmail.com>
2025-11-28 16:18:13 +0000

    Interaction outils debut

--- Triomphe_work/Header.js
+++ Triomphe_work/Header.js
@@ -24,3 +24,21 @@ window.addEventListener('scroll', function() {
         header.classList.remove('sticky');
     }
 });
+
+// Effet box outils
+ let boxOutil = document.querySelectorAll('list-outil');
+
+ boxOutil.forEach(item => {
+    item.addEventListener('mouseover', Fonction1);
+    item.addEventListener('mouseout', Fonction2);
+ });
+
+ function Fonction1(){
+    this.style.height = '310px';
+    this.style.margin = '5px';
+ }
+
+  function Fonction2(){
+    this.style.height = '300px';
+    this.style.margin = '10px';
+ }
